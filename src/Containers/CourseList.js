941960e8e3e9950bb5e18e098c5c891bdec71f9b
Michael Huang <huang.zha@husky.neu.edu>
2018-05-19 21:29:53 -0400

    Added Course Services

--- src/Containers/CourseList.js
+++ src/Containers/CourseList.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import CourseCard from "../Compnents/CourseCard";
 import "../Style.css"
+import CourseService from "../Services/CourseService";
 
 export default class CourseList
     extends React.Component {
+    constructor(){
+        super();
+        this.courseService = CourseService.instance
+    }
+    componentDidMount() {
+        this.findAllCourses();
+    }
+    findAllCourses() {
+        this.courseService
+            .findAllCourses()
+            .then((courses) => {
+                console.log(courses);
+                this.setState({courses: courses});
+            })
+    }
     render() {
         return (
             <div className="container-fluid">
